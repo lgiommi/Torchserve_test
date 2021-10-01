a891d6e77ddaba7ed2cc6f67e60b59d6c1fca7cb
lgiommi <luca.giommi2@studio.unibo.it>
2021-10-01 17:42:00 +0200

    updated files

--- flask/client.py
+++ flask/client.py
@@ -1,4 +1,4 @@
 import requests
-load = requests.post("http://localhost:5000/upload", files={"model": open('../pytorch_model.pt','rb')})
-resp = requests.post("http://localhost:5000/predict", files={"input": open('../predict.json','r')}, data={'model':'pytorch_model.pt'})
+load = requests.post("http://localhost:5000/upload", files={"model": open('pytorch_model.pt','rb')})
+resp = requests.post("http://localhost:5000/predict", files={"input": open('predict.json','r')}, data={'model':'pytorch_model.pt'})
 print(resp.text)
